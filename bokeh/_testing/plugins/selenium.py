4069d11a1e6c80a061d8ac74b0116833c44f2878
Bryan Van de Ven <bryanv@anaconda.com>
2018-07-26 13:17:47 -0700

    another try

--- bokeh/_testing/plugins/selenium.py
+++ bokeh/_testing/plugins/selenium.py
@@ -58,7 +58,6 @@ def driver(pytestconfig):
         options = Options()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920x1080")
         driver = webdriver.Chrome(chrome_options=options)
 
